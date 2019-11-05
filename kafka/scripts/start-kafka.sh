05a3191f086bdcc5e6ccddf8f38ada63563d61ca
Markward Schubert <markward.schuber@rewe-digital.com>
2019-11-05 18:53:49 +0100

    Excluded KAFKA_VERSION from being automatically inserted in config

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -56,7 +56,7 @@ fi
 
 for VAR in `env`
 do
-  if [[ $VAR =~ ^KAFKA_ && ! $VAR =~ ^KAFKA_HOME ]]; then
+  if [[ $VAR =~ ^KAFKA_ && ! $VAR =~ ^KAFKA_HOME && ! $VAR =~ ^KAFKA_VERSION ]]; then
     kafka_name=`echo "$VAR" | sed -r "s/KAFKA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
     env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
     if egrep -q "(^|^#)$kafka_name=" $KAFKA_HOME/config/server.properties; then
